fcfa1094cbced22a5310d818fb15a220aebb3efe
Sergio Gutierrez <ser-1998@hotmail.com>
2022-02-02 02:13:28 -0500

    .

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,14 +4,9 @@ from subprocess import PIPE
 from sys import stderr, stdout
 
 print("Este es el despues de instalar el template")
-project_packages = "{{ cookiecutter.project_packages }}"
+#dir_here = os.getcwd()
+#dir_requirementes = os.path.join(dir_here, os.pardir, "{{ cookiecutter.project_slug }}","requirements.txt")
 
-print(project_packages)
-dir_here = os.getcwd()
-dir_requirementes = os.path.join(dir_here, os.pardir, "{{ cookiecutter.project_slug }}","requirements.txt")
-
-if project_packages == 1:
-    subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
-
-else:
-    pass
+if '{{ cookiecutter.project_packages }}' == 'All':
+    print("hola")
+    subprocess.call(["pip", "install", "-r", "requirements.txt"])
